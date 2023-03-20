6778916e7bff59e0d8ae477edeef0346e7ab6b28
CathleenWM <cathleen5068@gmail.com>
2023-03-20 15:17:52 +0100

    start of opg 4

--- vue/my-first-vue-project/src/App.vue
+++ vue/my-first-vue-project/src/App.vue
@@ -1,8 +1,34 @@
 <script setup>
+
+import { ref } from 'vue';
+const email = ref('');
+const fornavn = ref('');
+const efternavn = ref('');
+const telefon = ref('');
+
 </script>
 
 <template>
   <div>
     Hejsa
   </div>
+
+  <div>
+    <p>Email: {{ email }}</p>
+    <input v-model="email" placeholder="Skriv din email">
+
+    <p>Fornavn: {{ fornavn }}</p>
+    <input v-model="fornavn" placeholder="Skriv dit fornavn">
+
+    <p>Efternavn: {{ efternavn }}</p>
+    <input v-model="efternavn" placeholder="Skriv dit efternavn">
+
+    <p>Telefon Nummer: {{ telefon }}</p>
+    <input v-model="telefon" placeholder="Skriv dit telefon nummer">
+
+  <h3>Navn: FORNAVN EFTERNAVN</h3>
+  <p>Email: EMAIL</p>
+  <p>Fødseldato: FØDSELSDATO</p>
+  </div>
+
 </template>
