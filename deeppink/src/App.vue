3b587074697c0efe370b46c7f3d8d06f52305891
CathleenWM <cathleen5068@gmail.com>
2023-03-20 14:22:31 +0100

    init

---
+++ deeppink/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue';
+
+const vandcykle = ref('');
+
+function onSubmit() {
+  alert(vandcykle.value);
+}
+
+</script>
+
+<template>
+
+  <!--Two way binding-->
+  <h1>Hello World</h1>
+  <input type="text" v-model="vandcykle"/>
+  <button v-on:click="onSubmit">
+    Dette er en knap
+  </button>
+  {{ vandcykle }}
+
+</template>
+
