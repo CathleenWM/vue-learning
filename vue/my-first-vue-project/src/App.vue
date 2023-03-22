16d3bad29ea2adcc14f097847bd05a2a6fb1f1b5
CathleenWM <cathleen5068@gmail.com>
2023-03-22 09:10:55 +0100

    Update App.vue

--- vue/my-first-vue-project/src/App.vue
+++ vue/my-first-vue-project/src/App.vue
@@ -12,13 +12,13 @@ const mail = ref ('');
 const telnr = ref ('');
 
 function onSubmit() {
-  alert(`navn: ${navn.value}, email: ${mail.value}, telefon nummer: ${telnr.value}`);
+  alert(`navn: ${navn.value}, \nemail: ${mail.value}, \ntelefon nummer: ${telnr.value}`);
 }
 
 </script>
 
 <template>
-  <div>
+  <!-- <div>
     Hejsa
   </div>
 
@@ -34,6 +34,19 @@ function onSubmit() {
 
     <p>Telefon Nummer: {{ telefon }}</p>
     <input v-model="telefon" placeholder="Skriv dit telefon nummer">
+  </div> -->
+
+  <div>
+    Form
   </div>
+  <br>
+
+  <form>
+    <input v-model="navn" type="text" placeholder="Skriv navn">
+    <br><br>
+    <input v-model="mail" type="text" placeholder="Skriv mail">
+    <br><br>
+    <input v-model="telnr" type="text" placeholder="Skriv nummer">
+  </form>
 
 </template>
